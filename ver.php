bc47b228f5a1882e1db104d13ed516f42c01f72f
Gerardo Tarragona <geratarra@gmail.com>
2015-12-09 18:46:11 -0700

    mostrando el proceso..

--- ver.php
+++ ver.php
@@ -1,8 +1,8 @@
 <?php
+    include("base.php");
     include("conexion.php");
     $sql = "select * from t2";
     $result = $conn->query($sql);
-
     if ($result->num_rows > 0) {
         $lista = array();
         while ($row = $result->fetch_assoc()) {
@@ -24,12 +24,13 @@
         $proceso_json += array("proceso" => $proceso["proceso"]);
         $proceso_json += array("id" => $proceso["p_id"]);
         $proceso_json += array("proposito" => $proceso["proposito_0"]);
+        $proceso_json += array("actividades" => array());
 
         $resultado = array();
         $tareas = array();
         foreach ($proceso as $key => $value) {
-            echo "Key=" . $key . ", Value=" . $value;
-            echo "<br>";
+            // echo "Key=" . $key . ", Value=" . $value;
+            // echo "<br>";
             if (strpos($key,'resultado') !== false) {
                 // if (array_key_exists('resultado', $proceso_json)) {
                 //     echo "entre";
@@ -43,31 +44,66 @@
                 array_push($resultado, $value);
             }
             if ($key[0] === 'a') {
-                echo "<br>siiii<br>";
-                echo $proceso[$key];
-                echo "<br>";
-                echo array_search($key, $proceso);
-                print_r(array_slice($proceso, array_search($key, $proceso)));
-                $nuevo = array_slice($proceso, array_search($key, $proceso));
-                // foreach ( as $key_tarea => $value_tarea) {
-                //     if ($key_tarea !== 'a'){
-                //         array_push($tareas, $value_tarea);
-                //     }
-                // }
+                // echo array_search($key, array_keys($proceso));
+                // print_r(array_slice($proceso, array_search($key, $proceso)));
+                $nuevo = array_slice($proceso, array_search($key, array_keys($proceso)) +1);
+                foreach ($nuevo as $key_tarea => $value_tarea) {
+                    if ($key_tarea !== 'a'){
+                        // array_push($tareas, array($key_tarea, $value_tarea));
+                        $tareas[$key_tarea] = $value_tarea;
+                    }
+                }
+                // $proceso_json["actividades"] += array("actividad" => array($value, $tareas));
+                // array_push($proceso_json["actividades"], array($value, $tareas));
+                $proceso_json["actividades"][$value] = $tareas;
+                $tareas = array();
                 // echo "<br>";
                 // print_r($nuevo);
             }
         }
+        echo "<body>";
+        echo "<div class=\"container\">";
         $proceso_json += array("resultado" => $resultado);
         // $proceso_json += array("actividad" => $tareas);
         // array_push($proceso_json, $resultado);
-        echo "<br>nuevooooo<br>";
-        // print_r($nuevo);
+        // echo "<br>nuevooooo<br>";
+        // print_r($tareas);
         echo "<br><br>";
-        // print_r($proceso_json);
+        echo "Proceso jason:<br>";
+        print_r($proceso_json);
+        echo "
+            <div class=\"page-header\">
+                <h1>".$proceso_json["proceso"]."</h1>
+            </div>";
+        echo "
+        <ul class=\"list-group\">
+            <li class=\"list-group-item\">
+                <h3>ID</h3>
+                <p>".$proceso_json[id]."</p>
+            </li>
+            <li class=\"list-group-item\">
+                <h3>Proposito</h3>
+                <p>".$proceso_json[proposito]."</p>
+            </li>";
+        echo "
+            <li class=\"list-group-item\">
+                <h3>Resultados esperados</h3>";
+        foreach($proceso_json[resultado] as $key_resultado => $value_resultado) {
+            echo "<p>".$key_resultado.": ".$value_resultado."</p><br>";
+        }
+        echo "</li>";
+
+        echo "<li class=\"list-group-item\">
+                <h3>Actividades</h3>";
+
+        echo "</li>";
+        echo "</ul>";
     }
 
     echo "<br>";
-    print_r($proceso);
+    // print_r($proceso);
     // echo $_GET["nombre"];
+    echo "</div>";
+    echo "</body>";
+    echo "</html>";
 ?>
